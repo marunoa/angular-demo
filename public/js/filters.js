691a1b45a59b37b978e99ebce74f90bfb100b98d
marunoa <marunoafp@gmail.com>
2016-09-17 18:49:47 -1000

    step18_model_bound_filters

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,6 +1,8 @@
 angular.module("myApp")
   .filter("beforeYearFilter", function () {
     return function (movies, year) {
-      return movies.filter(movie => movie.year < year);
+      return movies.filter(function (movie) {
+        return movie.year < year
+      });
     }
   })
