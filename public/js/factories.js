2e445d023b9b8f25a716ffe14cf91e24866ae1ce
marunoa <marunoafp@gmail.com>
2016-09-17 14:59:58 -1000

    step11_factory

---
+++ public/js/factories.js
@@ -0,0 +1,4 @@
+angular.module("myApp")
+  .factory("CharacterVersionFactory", ["mainCharacter", "APP_VERSION", function (mainCharacter, APP_VERSION) {
+    return `${mainCharacter} ${APP_VERSION}`;
+  }]);
