0efa53c728aa9c706d80d3f857e77da4487e906d
marunoa <marunoafp@gmail.com>
2016-09-17 19:47:19 -1000

    step20_routing

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -23,5 +23,31 @@ angular.module("myApp")
           Movies.getMovies()
             .then(function (response) {
               $scope.movies = response.data;
-            })
+            });
+  }])
+  .controller(
+    "booksController",
+      [ "$scope",
+        "BookService",
+        function (
+          $scope,
+          BookService
+        )
+        {
+          $scope.BookService = BookService;
+  }])
+  .controller(
+    "moviesController",
+      [ "$scope",
+        "Movies",
+        function (
+          $scope,
+          Movies
+        )
+        {
+          $scope.movies = [];
+          Movies.getMovies()
+            .then(function (response) {
+              $scope.movies = response.data;
+            });
   }])
--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,6 +1,22 @@
-var myApp = angular.module("myApp", [])
-  .config(["MoviesProvider", function (MoviesProvider) {
+var myApp = angular.module("myApp", ["ngRoute"])
+  .config(["MoviesProvider", "$routeProvider", function (MoviesProvider, $routeProvider) {
     MoviesProvider.setEndpoint("/api/movies");
+    $routeProvider
+      .when("/", {
+        templateUrl: "/views/default.html"
+      })
+      .when("/books", {
+        templateUrl: "/views/books.html",
+        controller: "booksController"
+      })
+       .when("/movies", {
+        templateUrl: "/views/movies.html",
+        controller: "moviesController"
+      })
+       .when("/other", {
+        templateUrl: "/views/other.html",
+        controller: "myController"
+      })
   }])
   .run(["$rootScope", "APP_VERSION", function ($rootScope, APP_VERSION) {
     $rootScope.APP_VERSION = APP_VERSION;
